343c10ccd2793e13dc0172f0f2845be3f9e9b310
KayJuno <dangdan2807@gmail.com>
2021-05-02 17:45:51 +0700

    update: database

--- src/database/database.sql
+++ src/database/database.sql
@@ -9,13 +9,15 @@ GO
 -- Tài khoản
 -- danh mục dịch vụ
 -- Dịch Vụ
+-- khách hàng
+-- Nhân viên
 -- Hóa đơn
 -- Chi tiết hóa đơn
 
 CREATE TABLE DanhMucPhong
 (
     maDanhMucPhong INT IDENTITY PRIMARY KEY,
-    tenLoaiPhong NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên',
+    tenLoaiPhong NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
     donGia FLOAT NOT NULL
 )
 GO
@@ -23,7 +25,7 @@ GO
 CREATE TABLE Phong
 (
     maPhong INT IDENTITY PRIMARY KEY,
-    tenPhong NVARCHAR(100) NOT NULL DEFAULT N'Phòng chưa đặt tên',
+    tenPhong NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
     sucChua INT NOT NULL DEFAULT(1),
     soGiuong INT NOT NULL DEFAULT(1),
     -- trống || có người
@@ -34,27 +36,49 @@ CREATE TABLE Phong
 )
 GO
 
+CREATE TABLE KhachHang
+(
+    maKH INT IDENTITY PRIMARY KEY,
+    tenKH NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
+    cmnd NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
+    -- 1. khách việt nam || 0. khách nước ngoài
+    loaiKhach INT NOT NULL DEFAULT 1
+)
+GO
+
+CREATE TABLE NhanVien
+(
+    maNV INT IDENTITY PRIMARY KEY,
+    tenNV NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
+    email NVARCHAR(100) DEFAULT N'Chưa cập nhật',
+    sdt NVARCHAR(100) DEFAULT N'Chưa cập nhật',
+)
+GO
+
 CREATE TABLE TaiKhoan
 (
-    UserName NVARCHAR(100) PRIMARY KEY,
-    DisplayName NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên',
+    userName NVARCHAR(100) PRIMARY KEY,
+    displayName NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
     PassWord VARCHAR(1000) NOT NULL DEFAULT 123456,
+    maNV INT NOT NULL,
     -- 1. admin || 0. nhân viên
-    loaiTK INT NOT NULL DEFAULT 0
+    loaiTK INT NOT NULL DEFAULT 0,
+
+    FOREIGN KEY (maNV) REFERENCES dbo.NhanVien (maNV)
 )
 GO
 
 CREATE TABLE DanhMucDichVu
 (
     maDanhMucDV INT IDENTITY PRIMARY KEY,
-    tenDanhMucDV NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên',
+    tenDanhMucDV NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
 )
 GO
 
 CREATE TABLE DichVu
 (
     maDV INT IDENTITY PRIMARY KEY,
-    tenDV NVARCHAR(100) NOT NULL DEFAULT N'chưa đặt tên',
+    tenDV NVARCHAR(100) NOT NULL DEFAULT N'Chưa cập nhật',
     maDanhMucDV INT NOT NULL,
     donGia FLOAT NOT NULL DEFAULT 0,
 
@@ -66,12 +90,16 @@ CREATE TABLE HoaDon
 (
     maHoaDon INT IDENTITY PRIMARY KEY,
     maPhong INT NOT NULL,
+    maKH INT NOT NULL,
+    maNV INT NOT NULL,
     ngayNhanPhong DATETIME DEFAULT getdate(),
     ngayTraPhong DATETIME NOT NULL,
     -- 1. đã thanh toán || 0. chưa thanh toán
     tinhTrang INT NOT NULL DEFAULT 0,
 
-    FOREIGN KEY (maPhong) REFERENCES dbo.Phong (maPhong)
+    FOREIGN KEY (maPhong) REFERENCES dbo.Phong (maPhong),
+    FOREIGN KEY (maKH) REFERENCES dbo.KhachHang (maKH),
+    FOREIGN KEY (maNV) REFERENCES dbo.NhanVien (maNV)
 )
 GO
 
@@ -86,3 +114,4 @@ CREATE TABLE ChiTietHoaDon
     FOREIGN KEY (maDV) REFERENCES dbo.DichVu (maDV)
 )
 GO
+-- DROP DATABASE QuanLyKhachSan
